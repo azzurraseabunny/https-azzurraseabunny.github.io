73e96e60363642797c6296057130dead310c30b9
ZurraBunny <97718893+azzurraseabunny@users.noreply.github.com>
2024-11-16 03:45:41 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://azzurraseabunny.github.io',
+    site: '',
 	base: '',
     integrations: [mdx(), sitemap(), react()],
 });
