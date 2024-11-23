962177dcf500a847c192074bfcc4866d9fbceb3b
ZurraBunny <rohnmaulawin24@gmail.com>
2024-11-23 13:40:15 +0800

    asdfasd

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,13 +1,16 @@
-// src/content/blog.ts
 import { defineCollection, z } from 'astro:content';
 
-// Define the 'blog' collection
-export const blog = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    pubDate: z.coerce.date(), // Ensures dates are coerced from strings
-    updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
-  }),
+const blog = defineCollection({
+	type: 'content',
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+	}),
 });
+
+export const collections = { blog };
